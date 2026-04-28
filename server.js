a27e40a78e3e2862e5505ad8cd59b496c1657fa9
Niyati-Rawal <your-email@example.com>
2026-04-28 21:36:55 +0530

    new commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
